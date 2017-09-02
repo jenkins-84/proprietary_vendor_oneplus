2233f05f6598368db8bea0d74b81129374a624c0
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-02 20:34:25 +0200

    cheeseburger: disable incompatible apps
    Change-Id: I44b0de59572c6ed6770f14be263945142d793d63

--- cheeseburger/cheeseburger-vendor.mk
+++ cheeseburger/cheeseburger-vendor.mk
@@ -764,14 +764,10 @@ PRODUCT_PACKAGES += \
     datastatusnotification \
     embms \
     shutdownlistener \
-    CNEService \
     QtiTetherService \
     com.qualcomm.location \
-    dpmserviceapp \
     qcrilmsgtunnel \
     colorservice \
-    ims \
-    imssettings \
     qcnvitems \
     qcrilhook
 endif
